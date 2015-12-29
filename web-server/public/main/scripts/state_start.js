f3751aad563aa2d7030bd35d7b403fa7b25071c0
hao <ahao0612@gmail.com>
2015-12-29 10:17:46 +0800

    fixx

--- web-server/public/main/scripts/state_start.js
+++ web-server/public/main/scripts/state_start.js
@@ -57,7 +57,7 @@ StateStart.prototype.start = function () {
     if (this.interval) {
     	clearInterval(this.interval);
     }
-    this.interval = setInterval(function(){ self.run(); }, 200);	
+    this.interval = setInterval(function(){ self.run(); }, 100);	
 };
 
 
